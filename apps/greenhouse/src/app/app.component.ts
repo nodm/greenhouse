5cbe28c6957c5966fe3191d5786556576550c392
Dmytro Novikov <nodm@Dmytros-MacBook-Pro.local>
2020-10-30 22:19:28 +0200

    initial commit

---
+++ apps/greenhouse/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'greenhouse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'greenhouse';
+}
